4ba725184da0c2b2bb1abcf1a8807a202a8973db
enxel <26006596+enxel@users.noreply.github.com>
2025-07-21 11:33:33 -0600

    Do exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reverse = [];
+    for (let i = 0; i < str.length; i++) {
+        reverse[str.length - 1 - i] = str[i];
+    }
 
+    return reverse.join("");
 };
 
 // Do not edit below this line
