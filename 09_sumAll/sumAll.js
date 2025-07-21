4c4f824f00d308b0f8c6f6a958213b8c7f16fa5f
enxel <26006596+enxel@users.noreply.github.com>
2025-07-21 12:08:59 -0600

    Do exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(n1,n2) {
+    if (!Number.isInteger(n1) || !Number.isInteger(n2) || n1 < 0 || n2 < 0) {
+        return "ERROR";
+    }
 
+    let acc = 0;
+    let a = (n1 <= n2) ? n1 : n2;
+    let b = (n1 <= n2) ? n2 : n1;
+
+    for (let i = a; i <= b; i++) {
+        acc += i;
+    }
+
+    return acc;
 };
 
 // Do not edit below this line
