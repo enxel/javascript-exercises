22240bf3d4c464decb58415e3a2d496abc9055d9
enxel <26006596+enxel@users.noreply.github.com>
2025-07-23 12:23:53 -0600

    Do exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const checker = item => isAlphanumeric(item);
+    return str.toLowerCase().split('').reverse().filter(checker).join('') === str.toLowerCase().split('').filter(checker).join('');
 };
+
+
+function isAlphanumeric(char) {
+  const code = char.charCodeAt(0);
+  return (
+    (code >= 48 && code <= 57)  || // 0-9
+    (code >= 65 && code <= 90)  || // A-Z
+    (code >= 97 && code <= 122)    // a-z
+  );
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
