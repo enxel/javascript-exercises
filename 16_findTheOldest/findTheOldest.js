80228ecc9384d12b6d5bad01e2a410d8ffc4fb35
enxel <26006596+enxel@users.noreply.github.com>
2025-07-23 14:12:34 -0600

    Do exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.sort(function (a, b) {
+        let aAge, bAge;
 
+        if (a.yearOfDeath === undefined) {
+            aAge = (new Date()).getFullYear() - a.yearOfBirth;
+        } else {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
+        if (b.yearOfDeath === undefined) {
+            bAge = (new Date()).getFullYear() - b.yearOfBirth;
+        } else {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+
+        return bAge - aAge;
+    })[0];
 };
 
 // Do not edit below this line
